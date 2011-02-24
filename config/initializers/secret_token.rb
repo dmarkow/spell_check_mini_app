7eeac31f4b8f86df692783ab347f6ea647c4a3aa
Dylan Markow <dm@petefowler.com>
2011-02-24 07:52:28 -0800

    New project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpellCheckMiniApp::Application.config.secret_token = '852870dd721a58f21cd53149c3cc22189610a546fe59946cfe32c91ee765a7f54a5ea9c7c55604cf5fcea40ce4438db109023edb8c67e2e15cefbd1e8d5deaa9'
